b53e859a0ac54e61d1f41435e54e86bf4e57e536
Shoaib <ShoaibSiddiqui0708@gmail.com>
2023-11-28 15:29:40 +0500

    3-Task-Done

--- src/Form/SignIn.js
+++ src/Form/SignIn.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className='form-parent'>
+      <form>
+        <h1>Sign In Form</h1>
+        <label>
+          Email:
+          <input type="email" name='email' placeholder='Enter Your Email' className='input-field' />
+        </label>
+        <label>
+          Password:
+          <input type="password" name='password' placeholder='Enter Your password' className='input-field' />
+        </label>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
 
--- src/Form/SignUp.js
+++ src/Form/SignUp.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='form-parent'>
+      <form>
+        <h1>Sign Up Form</h1>
+        <label>
+          Name:
+          <input type='text' name='text' placeholder='Enter Your Name' className='input-field' />
+        </label>
+        <label>
+          Email:
+          <input type="email" name='email' placeholder='Enter Your Email' className='input-field' />
+        </label>
+        <label>
+          Password:
+          <input type="password" name='password' placeholder='Enter Your password' className='input-field' />
+        </label>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
 
