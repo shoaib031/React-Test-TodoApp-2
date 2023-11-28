7f0322b5291f95e6d1c7ee51d9ed9c767220ae40
Shoaib <ShoaibSiddiqui0708@gmail.com>
2023-11-28 15:25:09 +0500

    2-Task-Done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-
-import Home from './todo/Home';
-import SignUp from './Form/SignUp'
-import SignIn from './Form/SignIn'
-import SignOut from './Form/SignOut'
-import AddTodo from './button/AddTodo'
-import UpdateTodo from './button/UpdateTodo'
-import DeleteTodo from './button/DeleteTodo'
+import Home from './todo/Home'
+import SignUpComponent from './Form/SignUp';
+import SignInComponent from './Form/SignIn';
+import SignOutComponent from './Form/SignOut';
+import AddTodo from './button/AddTodo';
+import UpdateTodo from './button/UpdateTodo';
+import DeleteTodo from './button/DeleteTodo';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
+  const handleSignUpClick = () => {
+    console.log('Clicked on Sign Up');
+  };
+
+  const handleSignInClick = () => {
+    console.log('Clicked on Sign In');
+  };
+
+  const handleSignOutClick = () => {
+    console.log('Clicked on Sign Out');
+  };
+
   return (
     <Router>
       <nav>
@@ -18,10 +28,13 @@ function App() {
             <Link to="/home">Home</Link>
           </li>
           <li>
-            <Link to="/signup">Sign Up</Link>
+            <Link to="/signup" onClick={handleSignUpClick}>Sign Up</Link>
           </li>
           <li>
-            <Link to="/signin">Sign In</Link>
+            <Link to="/signin" onClick={handleSignInClick}>Sign In</Link>
+          </li>
+          <li>
+            <Link to="/signout" onClick={handleSignOutClick}>Sign Out</Link>
           </li>
           <li>
             <Link to="/addtodo">Add Todo</Link>
@@ -32,20 +45,17 @@ function App() {
           <li>
             <Link to="/deletetodo">Delete Todo</Link>
           </li>
-          <li>
-            <Link to="/signout">Sign Out</Link>
-          </li>
         </ul>
       </nav>
 
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path='/home' element={<Home />} />
+        <Route path="/signup" element={<SignUpComponent />} />
+        <Route path="/signin" element={<SignInComponent />} />
+        <Route path="/signout" element={<SignOutComponent />} />
         <Route path="/addtodo" element={<AddTodo />} />
         <Route path="/updatetodo" element={<UpdateTodo />} />
         <Route path="/deletetodo" element={<DeleteTodo />} />
-        <Route path="/signout" element={<SignOut />} />
       </Routes>
     </Router>
   );
